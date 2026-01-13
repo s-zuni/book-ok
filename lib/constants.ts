ee95150ac43429392b02afa8c4f0d60fc1712f24
s-zuni <zxzx729@gmail.com>
2026-01-13 16:44:42 +0900

    1

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,10 +14,10 @@ export const RECOMMENDATION_TABS: Record<string, { label: string; query: string;
         { label: '국립어린이청소년도서관', query: '국립어린이청소년도서관' }
     ],
     '연령별 추천 도서': [
-        { label: '영유아 (0-4세)', query: '0-4세', categoryId: '4123' },
-        { label: '유치 (5-7세)', query: '5-7세', categoryId: '4123' },
-        { label: '초등 저학년 (8-10세)', query: '초등1-2학년', categoryId: '1108' },
-        { label: '초등 고학년 (11-13세)', query: '초등3-6학년', categoryId: '1108' }
+        { label: '영유아 (0-4세)', query: '영유아', categoryId: '4123' },
+        { label: '유치 (5-7세)', query: '유아', categoryId: '4123' },
+        { label: '초등 저학년 (8-10세)', query: '초등저학년', categoryId: '1108' },
+        { label: '초등 고학년 (11-13세)', query: '초등고학년', categoryId: '1108' }
     ],
     '수상 도서작': [
         { label: '전체', query: '문학상' },
